2e5b03a3da8056198aa711d0b0406b1592e40775
unknown <thecrowdel@gmail.com>
2022-05-31 16:58:51 +0700

    initial commit

---
+++ components/Footer.jsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <footer>Copyright 2022 Pares</footer>;
+};
+
+export default Footer;
---
+++ pages/about.jsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>All Listing | About</title>
+        <meta name="keyword" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi, quam.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
